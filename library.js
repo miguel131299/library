ebd9a0927376afeefc0cbed70597e2b4ab292025
miguel <miguel131299@gmail.com>
2021-10-06 13:14:46 +0200

    Fix Delete Functionailty

--- library.js
+++ library.js
@@ -7,73 +7,113 @@ const addButton = document.getElementById("new-book-button");
 //add Event Listeners
 addButton.addEventListener("click", addBookToLibrary);
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+function Book(title, author, pages, read, index) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.index = index;
 
-    this.info = function () {
-        let read_string;
+  this.info = function () {
+    let read_string;
 
-        if (read) {
-            read_string = "already read";
-        } else {
-            read_string = "not read yet";
-        }
-
-        return title + " by " + author + ", " + pages + " pages, " + read_string;
+    if (read) {
+      read_string = "already read";
+    } else {
+      read_string = "not read yet";
     }
+
+    return title + " by " + author + ", " + pages + " pages, " + read_string;
+  };
 }
 
 function addBookToLibrary(params) {
-    let title = window.prompt("Title: ")
-    let author = window.prompt("Author: ")
-    let pages = window.prompt("Number of Pages: ")
-    let read_answer = window.prompt("Have you read the book? Please answer yes or no: ")
+  let title = window.prompt("Title: ");
+  let author = window.prompt("Author: ");
+  let pages = window.prompt("Number of Pages: ");
+  let read_answer = window.prompt(
+    "Have you read the book? Please answer yes or no: "
+  );
 
-    let read;
-    if (read_answer === "yes") {
-        read = true;
-    }
-    else {
-        read = false;
-    }
+  let read;
+  if (read_answer === "yes") {
+    read = true;
+  } else {
+    read = false;
+  }
 
-    const book = new Book(title, author, pages, read);
+  const book = new Book(title, author, pages, read, myLibrary.length);
 
-    myLibrary.push(book);
+  myLibrary.push(book);
+
+  printLibraryInfo();
 }
 
 function printLibraryInfo() {
-    for (const book of myLibrary) {
-        const row = table.insertRow();
+  // delete previous table
+  table.innerHTML = `<tr>
+  <th>Title</th>
+  <th>Author</th>
+  <th>Pages</th>
+  <th>Read</th>
+  <th>ID</th>
+</tr>`;
 
-        for (const key in book) {
-            if (Object.hasOwnProperty.call(book, key)) {
+  let index = 0;
 
-                if (!isFunction(book[key])) {
+  for (const book of myLibrary) {
+    const row = table.insertRow();
 
-                    const cell = row.insertCell();
-                    cell.innerHTML = book[key];
-                }
-            }
+    for (const key in book) {
+      if (Object.hasOwnProperty.call(book, key)) {
+        if (!isFunction(book[key])) {
+          const cell = row.insertCell();
+          cell.innerHTML = book[key];
         }
-
-        const deleteButton = document.createElement("button");
-        deleteButton.innerHTML = "Delete";
-        row.appendChild(deleteButton);
+      }
     }
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "Delete";
+    row.appendChild(deleteButton);
+    deleteButton.addEventListener("click", (book) => {
+      deleteBook(book.index);
+    });
+
+    index++;
+  }
+}
+
+function deleteBook(index) {
+  console.log(index);
+  myLibrary.splice(index, 1);
+  printLibraryInfo();
 }
 
 function isFunction(functionToCheck) {
-    return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
-   }
+  return (
+    functionToCheck && {}.toString.call(functionToCheck) === "[object Function]"
+  );
+}
 
-const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-const book2 = new Book("Harry Potter", "J.K. Rowling", 456, true);
+const book1 = new Book(
+  "The Hobbit",
+  "J.R.R. Tolkien",
+  295,
+  false,
+  myLibrary.length
+);
 
 myLibrary.push(book1);
+
+const book2 = new Book(
+  "Harry Potter",
+  "J.K. Rowling",
+  456,
+  true,
+  myLibrary.length
+);
+
 myLibrary.push(book2);
 
 // addBookToLibrary();
